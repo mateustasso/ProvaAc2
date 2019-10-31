d01eac67f8631c4bbc6d616bbe840a7405f755b1
mateustasso <mateustassosiqueira@hotmail.com>
2019-10-30 22:15:47 -0200

    crossorigin

--- src/main/java/br/com/drummond/resources/Ac2Resources.java
+++ src/main/java/br/com/drummond/resources/Ac2Resources.java
@@ -9,13 +9,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-
+@CrossOrigin
 @RestController
 public class Ac2Resources {
 	
